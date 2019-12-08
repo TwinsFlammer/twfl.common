2f09ce0123f97cf7bf5d24c710854e40b71da8f4
Gutyerrez <srgutyerrez@gmail.com>
2019-12-08 06:42:10 -0300

    Created Mongo

---
+++ src/main/java/com/redecommunity/common/shared/databases/mongo/data/Mongo.java
@@ -0,0 +1,10 @@
+package com.redecommunity.common.shared.databases.mongo.data;
+
+import com.mongodb.client.MongoDatabase;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class Mongo {
+    private MongoDatabase mongoDatabase;
+}
