3a3daaf900d194b9005ba93b077eba02b5a325b7
Gutyerrez <srgutyerrez@gmail.com>
2019-12-08 06:43:27 -0300

    Updated Common

--- src/main/java/com/redecommunity/common/Common.java
+++ src/main/java/com/redecommunity/common/Common.java
@@ -1,18 +1,27 @@
 package com.redecommunity.common;
 
 import com.redecommunity.common.shared.databases.manager.DatabaseManager;
+import com.redecommunity.common.shared.scheduler.SchedulerManager;
+
+import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * Created by @SrGutyerrez
  */
 public class Common {
     private static Common instance;
+
+    /**
+     * All project static manager
+     */
     private DatabaseManager databaseManager;
+    private SchedulerManager schedulerManager;
 
     public Common() {
         Common.instance = this;
 
         this.databaseManager = new DatabaseManager();
+        this.schedulerManager = new SchedulerManager();
     }
 
     public static Common getInstance() {
@@ -21,5 +30,9 @@ public class Common {
 
     public DatabaseManager getDatabaseManager() {
         return this.databaseManager;
+    }
+
+    public ScheduledExecutorService getScheduler() {
+        return this.schedulerManager.getScheduledExecutorService();
     }
 }
