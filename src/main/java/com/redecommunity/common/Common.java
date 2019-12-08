5f1feca8d116ecb90c0a309fd7de9546b725a322
Gutyerrez <srgutyerrez@gmail.com>
2019-12-08 02:34:22 -0300

    Update Common.java

--- src/main/java/com/redecommunity/common/Common.java
+++ src/main/java/com/redecommunity/common/Common.java
@@ -1,16 +1,25 @@
 package com.redecommunity.common;
 
+import com.redecommunity.common.shared.databases.manager.DatabaseManager;
+
 /**
  * Created by @SrGutyerrez
  */
 public class Common {
     private static Common instance;
+    private DatabaseManager databaseManager;
 
     public Common() {
         Common.instance = this;
+
+        this.databaseManager = new DatabaseManager();
     }
 
     public static Common getInstance() {
         return Common.instance;
+    }
+
+    public DatabaseManager getDatabaseManager() {
+        return this.databaseManager;
     }
 }
