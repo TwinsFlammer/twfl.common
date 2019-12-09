fa59d64c80af42f8758b02a3c855f79f9f6d99e0
Gutyerrez <srgutyerrez@gmail.com>
2019-12-09 05:42:35 -0300

    Create Group.java

---
+++ src/main/java/com/redecommunity/common/shared/permissions/group/data/Group.java
@@ -0,0 +1,23 @@
+package com.redecommunity.common.shared.permissions.group.data;
+
+import com.redecommunity.common.shared.permissions.permission.data.Permission;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.awt.*;
+import java.util.Set;
+
+/**
+ * Created by @SrGutyerrez
+ */
+@RequiredArgsConstructor
+@Getter
+public class Group {
+    private final Integer id;
+    private final String name, prefix, suffix;
+    private final Color color;
+    private final Integer priority, tabListListOrder;
+    private final Long discordGroupId;
+    private final Integer serverId;
+    private final Set<Permission> permissions;
+}
