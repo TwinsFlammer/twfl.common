cdc5b899572c1c99bd06090da54d21b9b8974777
Gutyerrez <srgutyerrez@gmail.com>
2019-12-10 08:39:50 -0300

    Update Common.java

--- src/main/java/com/redecommunity/common/Common.java
+++ src/main/java/com/redecommunity/common/Common.java
@@ -1,6 +1,7 @@
 package com.redecommunity.common;
 
 import com.redecommunity.common.shared.databases.manager.DatabaseManager;
+import com.redecommunity.common.shared.language.factory.LanguageFactory;
 import com.redecommunity.common.shared.manager.GlobalManager;
 import com.redecommunity.common.shared.scheduler.SchedulerManager;
 
@@ -12,17 +13,27 @@ import java.util.concurrent.ScheduledExecutorService;
 public class Common {
     private static Common instance;
 
+    public static final String SERVER_HOME = "/home/redecommunity";
+
     /**
      * All project static manager
      */
     private DatabaseManager databaseManager;
     private SchedulerManager schedulerManager;
 
+    /**
+     * All project static factory
+     */
+
+    private LanguageFactory languageFactory;
+
     public Common() {
         Common.instance = this;
 
         this.schedulerManager = new SchedulerManager();
         this.databaseManager = new DatabaseManager();
+
+        this.languageFactory = new LanguageFactory();
 
         new GlobalManager();
     }
@@ -41,5 +52,9 @@ public class Common {
 
     public ScheduledExecutorService getScheduler() {
         return this.schedulerManager.getScheduledExecutorService();
+    }
+
+    public LanguageFactory getLanguageFactory() {
+        return this.languageFactory;
     }
 }
