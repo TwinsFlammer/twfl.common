e2dbf2ceda78575aab5da85b0a6a1b736f3bdca0
Gutyerrez <srgutyerrez@gmail.com>
2019-12-11 01:20:28 -0300

    Update DatabaseManager.java

--- src/main/java/com/redecommunity/common/shared/databases/manager/DatabaseManager.java
+++ src/main/java/com/redecommunity/common/shared/databases/manager/DatabaseManager.java
@@ -2,7 +2,9 @@ package com.redecommunity.common.shared.databases.manager;
 
 import com.redecommunity.common.shared.Common;
 import com.redecommunity.common.shared.databases.configuration.DatabaseConfiguration;
+import com.redecommunity.common.shared.databases.mongo.manager.MongoManager;
 import com.redecommunity.common.shared.databases.mysql.manager.MySQLManager;
+import com.redecommunity.common.shared.databases.redis.manager.RedisManager;
 import com.redecommunity.common.shared.databases.runnable.DatabaseRefreshRunnable;
 
 import java.util.concurrent.TimeUnit;
@@ -15,11 +17,15 @@ public class DatabaseManager {
     private DatabaseConfiguration databaseConfiguration;
 
     private MySQLManager mySQLManager;
+    private RedisManager redisManager;
+    private MongoManager mongoManager;
 
     public DatabaseManager() {
-        setupDatabaseConfiguration();
+        this.databaseConfiguration = new DatabaseConfiguration();
 
         this.mySQLManager = new MySQLManager(this.databaseConfiguration);
+        this.redisManager = new RedisManager(this.databaseConfiguration);
+        this.mongoManager = new MongoManager(this.databaseConfiguration);
 
         this.refresh();
 
@@ -37,13 +43,19 @@ public class DatabaseManager {
 
     public void refresh() {
         this.mySQLManager.refresh();
+        this.redisManager.refresh();
+        this.mongoManager.refresh();
     }
 
     public MySQLManager getMySQLManager() {
         return this.mySQLManager;
     }
 
-    private void setupDatabaseConfiguration() {
-        this.databaseConfiguration = new DatabaseConfiguration();
+    public RedisManager getRedisManager() {
+        return this.redisManager;
+    }
+
+    public MongoManager getMongoManager() {
+        return this.mongoManager;
     }
 }
