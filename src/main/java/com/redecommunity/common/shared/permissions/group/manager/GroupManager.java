9776029b7ffcf121bf76d6394b8f2dddaceaa7e4
Gutyerrez <srgutyerrez@gmail.com>
2019-12-14 03:00:05 -0300

    Update GroupManager.java

--- src/main/java/com/redecommunity/common/shared/permissions/group/manager/GroupManager.java
+++ src/main/java/com/redecommunity/common/shared/permissions/group/manager/GroupManager.java
@@ -1,8 +1,12 @@
 package com.redecommunity.common.shared.permissions.group.manager;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.redecommunity.common.shared.permissions.group.data.Group;
 
+import java.awt.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
 
 /**
@@ -25,5 +29,20 @@ public class GroupManager {
                 .filter(group -> group.getName().equalsIgnoreCase(name))
                 .findFirst()
                 .orElse(null);
+    }
+
+    public static Group toGroup(ResultSet resultSet) throws SQLException {
+        return new Group(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("prefix"),
+                resultSet.getString("suffix"),
+                Color.getColor(resultSet.getString("color")),
+                resultSet.getInt("priority"),
+                resultSet.getInt("tab_list_order"),
+                resultSet.getLong("discord_group_id"),
+                resultSet.getInt("server_id"),
+                Sets.newConcurrentHashSet()
+        );
     }
 }
