11dd539833f8e02b9769b51e5ae192dca1a0dce9
Gutyerrez <srgutyerrez@gmail.com>
2019-12-14 06:42:10 -0300

    Update GlobalManager.java

--- src/main/java/com/redecommunity/common/shared/manager/GlobalManager.java
+++ src/main/java/com/redecommunity/common/shared/manager/GlobalManager.java
@@ -4,6 +4,9 @@ import com.redecommunity.common.shared.Common;
 import com.redecommunity.common.shared.databases.mysql.dao.Table;
 import com.redecommunity.common.shared.util.ClassGetter;
 
+import java.util.Collection;
+import java.util.Collections;
+
 /**
  * Created by @SrGutyerrez
  */
@@ -19,9 +22,11 @@ public class GlobalManager {
 
 class TableManager {
     TableManager() {
+        Collection<Class> blacklisted = Collections.singletonList(Table.class);
+
         for (Class<?> clazz : ClassGetter.getClassesForPackage(Common.class)) {
             try {
-                if (Table.class.isAssignableFrom(clazz)) {
+                if (Table.class.isAssignableFrom(clazz) && !blacklisted.contains(clazz)) {
                     Table table = (Table) clazz.newInstance();
 
                     table.createTable();
