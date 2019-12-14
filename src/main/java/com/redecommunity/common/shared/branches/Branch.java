cff9364c5cbc86a0d3fbf15390100fdb24e7b000
Gutyerrez <srgutyerrez@gmail.com>
2019-12-13 22:59:47 -0300

    Create Branch.java

---
+++ src/main/java/com/redecommunity/common/shared/branches/Branch.java
@@ -0,0 +1,27 @@
+package com.redecommunity.common.shared.branches;
+
+import com.redecommunity.common.shared.Common;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.io.File;
+
+/**
+ * Created by @SrGutyerrez
+ */
+@RequiredArgsConstructor
+@Getter
+public enum Branch {
+    MASTER("master"),
+    DEVELOP("develop");
+
+    private final String name;
+
+    public File getPath() {
+        File file = new File(Common.SERVER_HOME + "/files/jars/" + this.name);
+
+        if (file.exists()) file.mkdirs();
+
+        return file;
+    }
+}
