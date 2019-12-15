323108b12e9b4d11f469d4db41efc1c9f1b28df8
Gutyerrez <srgutyerrez@gmail.com>
2019-12-15 12:07:16 -0300

    Update Channel.java

--- src/main/java/com/redecommunity/common/shared/databases/redis/channel/data/Channel.java
+++ src/main/java/com/redecommunity/common/shared/databases/redis/channel/data/Channel.java
@@ -4,10 +4,6 @@ package com.redecommunity.common.shared.databases.redis.channel.data;
  * Created by @SrGutyerrez
  */
 public abstract class Channel implements IChannel {
-    public Channel(String name) {
-
-    }
-
     @Override
     public abstract String getName();
 }
