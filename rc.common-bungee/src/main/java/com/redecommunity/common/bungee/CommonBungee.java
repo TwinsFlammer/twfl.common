20825f4e9232547f1bbf65864af4a29561e65fcb
Gutyerrez <srgutyerrez@gmail.com>
2019-12-16 05:54:56 -0300

    Create CommonBungee.java

---
+++ rc.common-bungee/src/main/java/com/redecommunity/common/bungee/CommonBungee.java
@@ -0,0 +1,38 @@
+package com.redecommunity.common.bungee;
+
+import com.redecommunity.common.shared.Common;
+import com.redecommunity.common.shared.updater.data.Updater;
+import net.md_5.bungee.api.plugin.Plugin;
+
+import java.io.IOException;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class CommonBungee extends Plugin {
+    private static CommonBungee instance;
+
+    public CommonBungee() {
+        CommonBungee.instance = this;
+    }
+
+    public static CommonBungee getInstance() {
+        return CommonBungee.instance;
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        Updater updater = new Updater(this.getFile(), Common.getBranch());
+
+        try {
+            updater.download();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
