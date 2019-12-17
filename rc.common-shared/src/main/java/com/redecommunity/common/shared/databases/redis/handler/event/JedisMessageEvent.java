38f3e2d461a3c124070830775114be1831a02105
Gutyerrez <srgutyerrez@gmail.com>
2019-12-17 12:13:27 -0300

    Update JedisMessageEvent.java

--- rc.common-shared/src/main/java/com/redecommunity/common/shared/databases/redis/handler/event/JedisMessageEvent.java
+++ rc.common-shared/src/main/java/com/redecommunity/common/shared/databases/redis/handler/event/JedisMessageEvent.java
@@ -1,11 +1,13 @@
 package com.redecommunity.common.shared.databases.redis.handler.event;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 /**
  * Created by @SrGutyerrez
  */
 @RequiredArgsConstructor
+@Getter
 public class JedisMessageEvent {
     private final String channel, message;
 }
