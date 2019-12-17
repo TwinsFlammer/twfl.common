cd6810c05305f30aded8cc550d967beddbfc9a3e
Gutyerrez <srgutyerrez@gmail.com>
2019-12-17 08:10:22 -0300

    Create GroupManager.java

---
+++ rc.common-shared/src/main/java/com/redecommunity/common/shared/permissions/group/manager/GroupManager.java
@@ -0,0 +1,82 @@
+package com.redecommunity.common.shared.permissions.group.manager;
+
+import com.google.common.collect.Lists;
+import com.redecommunity.common.shared.permissions.group.data.Group;
+import com.redecommunity.common.shared.permissions.permission.dao.PermissionDao;
+import com.redecommunity.common.shared.permissions.permission.data.Permission;
+import com.redecommunity.common.shared.permissions.group.dao.GroupDao;
+
+import java.awt.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class GroupManager {
+    private static Collection<Group> groups = Lists.newArrayList();
+
+    public GroupManager() {
+        GroupDao groupDao = new GroupDao();
+        PermissionDao permissionDao = new PermissionDao();
+
+        GroupManager.setGroups(groupDao.findAll());
+
+        Set<Permission> permissions = permissionDao.findAll();
+
+        permissions.forEach(permission -> {
+            Integer groupId = permission.getGroupId();
+
+            Group group = GroupManager.getGroup(groupId);
+
+            group.getPermissions().add(permission);
+
+            if (permission.grantToHigher())
+                GroupManager.groups
+                        .stream()
+                        .filter(group1 -> group1.getPriority() > group.getPriority())
+                        .forEach(group1 -> group1.getPermissions().add(permission));
+        });
+    }
+
+    public static Collection<Group> getGroups() {
+        return GroupManager.groups;
+    }
+
+    public static void setGroups(Collection<Group> groups) {
+        GroupManager.groups = groups;
+    }
+
+    public static Group getGroup(Integer id) {
+        return GroupManager.groups
+                .stream()
+                .filter(group -> group.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static Group getGroup(String name) {
+        return GroupManager.groups
+                .stream()
+                .filter(group -> group.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static Group toGroup(ResultSet resultSet) throws SQLException {
+        return new Group(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("prefix"),
+                resultSet.getString("suffix"),
+                Color.getColor(resultSet.getString("color")),
+                resultSet.getInt("priority"),
+                resultSet.getInt("tab_list_order"),
+                resultSet.getLong("discord_group_id"),
+                resultSet.getInt("server_id"),
+                Lists.newArrayList()
+        );
+    }
+}
