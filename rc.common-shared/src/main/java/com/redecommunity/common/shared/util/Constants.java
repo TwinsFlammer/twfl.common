dd1af296d573e36c4c252348be810fd1afa586fa
Gutyerrez <srgutyerrez@gmail.com>
2019-12-18 11:42:24 -0300

    Create Constants.java

---
+++ rc.common-shared/src/main/java/com/redecommunity/common/shared/util/Constants.java
@@ -0,0 +1,8 @@
+package com.redecommunity.common.shared.util;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class Constants {
+    public static final String MESSAGE_CHANNEL = "message_channel";
+}
