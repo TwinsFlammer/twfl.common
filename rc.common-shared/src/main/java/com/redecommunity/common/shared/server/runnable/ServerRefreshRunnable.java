443dc1ba7ec2e34548b3eefad55cf993c425710d
Gutyerrez <srgutyerrez@gmail.com>
2019-12-21 14:44:19 -0300

    Update ServerRefreshRunnable.java

--- rc.common-shared/src/main/java/com/redecommunity/common/shared/server/runnable/ServerRefreshRunnable.java
+++ rc.common-shared/src/main/java/com/redecommunity/common/shared/server/runnable/ServerRefreshRunnable.java
@@ -19,11 +19,13 @@ public class ServerRefreshRunnable implements Runnable {
         servers.forEach(server -> {
             Server server1 = ServerManager.getServer(server.getId());
 
-            if (server1 != null && !server1.isSimilar(server)) {
-                server1.updateData(server);
+            if (server1 != null) {
+                if (!server1.isSimilar(server)) server1.updateData(server);
+
                 server1.update();
-            } else if (server1 == null) {
+            } else {
                 server.update();
+
                 ServerManager.addServer(server);
             }
         });
