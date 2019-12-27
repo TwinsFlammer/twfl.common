d3c81f1e130b55117981ec73f4ec7a1564ba8137
Gutyerrez <srgutyerrez@gmail.com>
2019-12-27 15:29:59 -0300

    Update Updater.java

--- rc.common-shared/src/main/java/com/redecommunity/common/shared/updater/data/Updater.java
+++ rc.common-shared/src/main/java/com/redecommunity/common/shared/updater/data/Updater.java
@@ -1,6 +1,7 @@
 package com.redecommunity.common.shared.updater.data;
 
 import com.redecommunity.common.shared.branches.Branch;
+import com.redecommunity.common.shared.util.Printer;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -20,18 +21,18 @@ public class Updater {
     private final Branch branch;
 
     public void download() throws IOException {
-        if (!this.file.exists()) throw new FileNotFoundException("Não foi possível localizar o arquivo " + file.getName());
-
         File remoteFile = new File(this.branch.getPath() + "/" + this.file.getName());
 
-        System.out.println("Verificando se há atualizações do arquivo " + this.file.getName());
+        if (!this.file.exists() || !remoteFile.exists()) throw new FileNotFoundException("Não foi possível localizar o arquivo " + file.getName());
+
+        Printer.INFO.coloredPrint("&eVerificando se há atualizações do arquivo " + this.file.getName());
 
         if (remoteFile.lastModified() > this.file.lastModified()) {
-            System.out.println("Atualização encontrada, baixando nova versão do arquivo " + this.file.getName());
+            Printer.INFO.coloredPrint("&eAtualização encontrada, baixando nova versão do arquivo " + this.file.getName());
 
             Files.copy(remoteFile.toPath(), this.file.toPath(), StandardCopyOption.REPLACE_EXISTING);
 
-            System.out.println("Arquivo " + this.file.getName() + " atualizado com sucesso!");
+            Printer.INFO.coloredPrint("&aArquivo " + this.file.getName() + " atualizado com sucesso!");
         }
     }
 }
