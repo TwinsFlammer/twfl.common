497d033f9d6e5a8f2c321ff6921c63e0ddb135e3
Gutyerrez <srgutyerrez@gmail.com>
2019-12-29 23:07:12 -0300

    Update Constants.java

--- rc.common-shared/src/main/java/com/redecommunity/common/shared/util/Constants.java
+++ rc.common-shared/src/main/java/com/redecommunity/common/shared/util/Constants.java
@@ -9,4 +9,6 @@ public class Constants {
     public static final String CONNECT_CHANNEL = "connect_channel";
     public static final String BROADCAST_MESSAGE = "broadcast_message_channel";
     public static final String PREFERENCE_CHANNEL = "preference_channel";
+    public static final String FRIEND_CHANNEL = "friend_channel";
+    public static final String IGNORE_CHANNEL = "ignore_channel";
 }
