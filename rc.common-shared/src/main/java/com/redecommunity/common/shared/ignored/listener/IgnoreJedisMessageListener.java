8dce801f64a9ba8ac8b1cc758f9c2762439d04d4
Gutyerrez <srgutyerrez@gmail.com>
2019-12-29 23:36:05 -0300

    Update IgnoreJedisMessageListener.java

--- rc.common-shared/src/main/java/com/redecommunity/common/shared/ignored/listener/IgnoreJedisMessageListener.java
+++ rc.common-shared/src/main/java/com/redecommunity/common/shared/ignored/listener/IgnoreJedisMessageListener.java
@@ -21,9 +21,11 @@ public class IgnoreJedisMessageListener implements JedisMessageListener {
 
         Integer userId = ((Long) jsonObject.get("user_id")).intValue();
         Integer ignoredId = ((Long) jsonObject.get("ignored_id")).intValue();
+        Boolean action = (Boolean) jsonObject.get("action");
 
         User user = UserManager.getUser(userId);
 
-        user.ignore(ignoredId);
+        if (action) user.ignore(ignoredId);
+        else user.unIgnore(ignoredId);
     }
 }
