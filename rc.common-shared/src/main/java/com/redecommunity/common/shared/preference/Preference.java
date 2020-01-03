a4755a4ff4f663810cecf62c5677ba1f0ec1f0cb
Gutyerrez <srgutyerrez@gmail.com>
2020-01-03 20:14:20 -0300

    Update Preference.java

--- rc.common-shared/src/main/java/com/redecommunity/common/shared/preference/Preference.java
+++ rc.common-shared/src/main/java/com/redecommunity/common/shared/preference/Preference.java
@@ -1,6 +1,7 @@
 package com.redecommunity.common.shared.preference;
 
 import com.google.common.collect.Sets;
+import com.redecommunity.common.shared.permissions.user.data.User;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -13,24 +14,116 @@ import java.util.Set;
  */
 @RequiredArgsConstructor
 public enum Preference {
-    VISIBILITY("visibility"),
-    LOBBY_PROTECTION("lobby_protection"),
-    BLOOD_EFFECT("blood_effect"),
-    MENTION_NOTIFICATIONS("mention_notifications"),
-    ACTION_BAR_ABILITIES("action_bar_abilities"),
-    CHAT_LOCAL("chat_local"),
-    CHAT_GLOBAL("chat_global"),
-    PERSONAL_MARKET("personal_market"),
-    TELEPORT_REQUEST("teleport_request"),
-    MONEY_RECEIVE("money_receive"),
-    UNDER_ATTACK_MESSAGE("under_attack_message"),
-    MONEY_RECEIVED_MESSAGE("money_received_message"),
-    COMBAT_MESSAGE("combat_message"),
-    JOIN_MESSAGE("join_message"),
-    PRIVATE_MESSAGE("private_message");
+    VISIBILITY(
+            "visibility",
+            "Visibilidade dos jogadores",
+            0,
+            0,
+            new String[]{}
+    ),
+    LOBBY_PROTECTION(
+            "lobby_protection",
+            "",
+            0,
+            0,
+            new String[]{}
+    ),
+    BLOOD_EFFECT(
+            "blood_effect",
+            "",
+            0,
+            0,
+            new String[]{}
+    ),
+    MENTION_NOTIFICATIONS(
+            "mention_notifications",
+            "",
+            0,
+            0,
+            new String[]{}
+    ),
+    ACTION_BAR_ABILITIES(
+            "action_bar_abilities",
+            "",
+            0,
+            0,
+            new String[]{}
+    ),
+    CHAT_LOCAL(
+            "chat_local",
+            "",
+            0,
+            0,
+            new String[]{}
+    ),
+    CHAT_GLOBAL(
+            "chat_global",
+            "",
+            0,
+            0,
+            new String[]{}
+    ),
+    PERSONAL_MARKET(
+            "personal_market",
+            "",
+            0,
+            0,
+            new String[]{}
+    ),
+    TELEPORT_REQUEST(
+            "teleport_request",
+            "",
+            0,
+            0,
+            new String[]{}
+    ),
+    MONEY_RECEIVE(
+            "money_receive",
+            "",
+            0,
+            0,
+            new String[]{}
+    ),
+    UNDER_ATTACK_MESSAGE(
+            "under_attack_message",
+            "",
+            0,
+            0,
+            new String[]{}
+    ),
+    MONEY_RECEIVED_MESSAGE(
+            "money_received_message",
+            "",
+            0,
+            0,
+            new String[]{}
+    ),
+    COMBAT_MESSAGE(
+            "combat_message",
+            "",
+            0,
+            0,
+            new String[]{}
+    ),
+    JOIN_MESSAGE(
+            "join_message",
+            "",
+            0,
+            0,
+            new String[]{}
+    ),
+    PRIVATE_MESSAGE(
+            "private_message",
+            "",
+            0,
+            0,
+            new String[]{}
+    );
 
     @Getter
-    private final String columnName;
+    private final String columnName, displayName;
+    private final Integer id, data;
+    private final String[] description;
 
     public static <T> Set<T> toPreference(ResultSet resultSet) throws SQLException {
         Set<T> preferences = Sets.newConcurrentHashSet();
@@ -39,5 +132,9 @@ public enum Preference {
             if (resultSet.getBoolean(preference.getColumnName())) preferences.add((T) preference);
 
         return preferences;
+    }
+
+    public String getColor(User user) {
+        return user.isEnabled(this) ? "a" : "4";
     }
 }
