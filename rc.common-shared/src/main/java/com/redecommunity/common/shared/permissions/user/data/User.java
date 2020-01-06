d1a5cd4e7ca8c030f4c1d0e389de7a5876702859
Gutyerrez <srgutyerrez@gmail.com>
2020-01-06 09:01:13 -0300

    Update User.java

--- rc.common-shared/src/main/java/com/redecommunity/common/shared/permissions/user/data/User.java
+++ rc.common-shared/src/main/java/com/redecommunity/common/shared/permissions/user/data/User.java
@@ -263,6 +263,8 @@ public class User {
         jsonObject.put("ignored_id", userId);
         jsonObject.put("action", true);
 
+        System.out.println("PORRa");
+
         this.getRedis().sendMessage(
                 Constants.IGNORE_CHANNEL,
                 jsonObject.toString()
