7fb823eef54561e553ffb071652bfd80bc7d80ae
Gutyerrez <srgutyerrez@gmail.com>
2020-01-14 03:51:49 -0300

    Create TimeFormatter.java

---
+++ rc.common-shared/src/main/java/com/redecommunity/common/shared/util/TimeFormatter.java
@@ -0,0 +1,50 @@
+package com.redecommunity.common.shared.util;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class TimeFormatter {
+    public static String format(long time) {
+        if (time == 0) return "nunca";
+
+        long day = TimeUnit.MILLISECONDS.toDays(time);
+        long hours = TimeUnit.MILLISECONDS.toHours(time) - (day * 24);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(time) - (hours * 60);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(time) - (minutes * 60);
+        long milliseconds = TimeUnit.MILLISECONDS.toMillis(time) - (seconds * 1000);
+
+        StringBuilder sb = new StringBuilder();
+
+        if (day > 0) {
+            sb.append(day)
+                    .append("d")
+                    .append(" ");
+        }
+        if (hours > 0) {
+            sb.append(hours)
+                    .append("h")
+                    .append(" ");
+        }
+        if (minutes > 0) {
+            sb.append(minutes)
+                    .append("m")
+                    .append(" ");
+        }
+        if (seconds > 0) {
+            sb.append(seconds)
+                    .append("s")
+                    .append(" ");
+        }
+        if (milliseconds > 0) {
+            sb.append(milliseconds)
+                    .append(String.format("%.1d", milliseconds))
+                    .append("s");
+        }
+
+        String diff = sb.toString();
+
+        return diff.isEmpty() ? "agora" : diff;
+    }
+}
