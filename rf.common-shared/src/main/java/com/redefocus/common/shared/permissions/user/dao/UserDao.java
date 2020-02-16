99189318408ac7cb3dab7d48840d0381d3116207
Gutyerrez <srgutyerrez@gmail.com>
2020-02-16 13:20:26 -0300

    Rename all package from redecommunity to redefocus

--- rf.common-shared/src/main/java/com/redefocus/common/shared/permissions/user/dao/UserDao.java
+++ rf.common-shared/src/main/java/com/redefocus/common/shared/permissions/user/dao/UserDao.java
@@ -1,12 +1,12 @@
-package com.redecommunity.common.shared.permissions.user.dao;
+package com.redefocus.common.shared.permissions.user.dao;
 
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import com.redecommunity.common.shared.permissions.user.data.User;
-import com.redecommunity.common.shared.databases.mysql.dao.Table;
-import com.redecommunity.common.shared.permissions.user.group.dao.UserGroupDao;
-import com.redecommunity.common.shared.permissions.user.group.data.UserGroup;
-import com.redecommunity.common.shared.permissions.user.manager.UserManager;
+import com.redefocus.common.shared.permissions.user.data.User;
+import com.redefocus.common.shared.databases.mysql.dao.Table;
+import com.redefocus.common.shared.permissions.user.group.dao.UserGroupDao;
+import com.redefocus.common.shared.permissions.user.group.data.UserGroup;
+import com.redefocus.common.shared.permissions.user.manager.UserManager;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
