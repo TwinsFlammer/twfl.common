6d408ecd70c69644ec5350e9193cf9df3670449d
Gutyerrez <srgutyerrez@gmail.com>
2020-02-16 13:22:31 -0300

    Rename all package from redecommunity to redefocus

--- rf.common-shared/src/main/java/com/redefocus/common/shared/permissions/user/report/dao/UserReportDao.java
+++ rf.common-shared/src/main/java/com/redefocus/common/shared/permissions/user/report/dao/UserReportDao.java
@@ -1,10 +1,10 @@
-package com.redecommunity.common.shared.permissions.user.report.dao;
+package com.redefocus.common.shared.permissions.user.report.dao;
 
 import com.google.common.collect.Sets;
-import com.redecommunity.common.shared.databases.mysql.dao.Table;
-import com.redecommunity.common.shared.permissions.user.data.User;
-import com.redecommunity.common.shared.report.data.ReportReason;
-import com.redecommunity.common.shared.report.manager.ReportReasonManager;
+import com.redefocus.common.shared.databases.mysql.dao.Table;
+import com.redefocus.common.shared.permissions.user.data.User;
+import com.redefocus.common.shared.report.data.ReportReason;
+import com.redefocus.common.shared.report.manager.ReportReasonManager;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
