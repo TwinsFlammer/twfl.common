61de2c9c5b7f30d250d2e595c08754bd94b097dd
Gutyerrez <srgutyerrez@gmail.com>
2020-03-06 06:57:05 -0300

    Update TimeFormatter.java

--- rc.common-shared/src/main/java/com/redecommunity/common/shared/util/TimeFormatter.java
+++ rc.common-shared/src/main/java/com/redecommunity/common/shared/util/TimeFormatter.java
@@ -1,22 +1,34 @@
 package com.redecommunity.common.shared.util;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by @SrGutyerrez
  */
 public class TimeFormatter {
+    public static void main(String[] args) {
+        Long time = 62500L;
+
+        String formatted = TimeFormatter.format(time);
+
+        System.out.println(formatted);
+    }
+
     public static String format(Long time) {
         if (time == 0) return "nunca";
 
-        Long day = TimeUnit.MILLISECONDS.toDays(time);
-        Long hours = TimeUnit.MILLISECONDS.toHours(time) - (day * 24);
+        Long days = TimeUnit.MILLISECONDS.toDays(time);
+        Long hours = TimeUnit.MILLISECONDS.toHours(time) - (days * 24);
         Long minutes = TimeUnit.MILLISECONDS.toMinutes(time) - (hours * 60);
         Long seconds = TimeUnit.MILLISECONDS.toSeconds(time) - (minutes * 60);
+        Long millis = TimeUnit.MILLISECONDS.toMillis(time) - (seconds * 60);
 
         StringBuilder stringBuilder = new StringBuilder();
 
-        if (day > 0) stringBuilder.append(day)
+        if (days > 0) stringBuilder.append(days)
                 .append("d")
                 .append(" ");
 
@@ -28,11 +40,25 @@ public class TimeFormatter {
                 .append("m")
                 .append(" ");
 
-        if (seconds > -1) stringBuilder.append(seconds)
+        if (days == 0 && hours == 0 && minutes == 0 && seconds == 0 && millis < 1000) {
+            double halfSeconds = millis / 1000D;
+
+            NumberFormat numberFormat = new DecimalFormat("#.#");
+            String value = numberFormat.format(TimeFormatter.roundDouble(halfSeconds));
+
+            stringBuilder.append(value)
+                    .append("s");
+        } if (seconds > 0) stringBuilder.append(seconds)
                 .append("s");
 
         String formatted = stringBuilder.toString();
 
         return formatted.isEmpty() ? "agora" : formatted;
+    }
+
+    private static double roundDouble(double base) {
+        return new BigDecimal(base)
+                .setScale(2, BigDecimal.ROUND_HALF_UP)
+                .doubleValue();
     }
 }
