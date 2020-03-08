ef26f9bf34f377168eba73958391a68ac6665cf6
Gutyerrez <srgutyerrez@gmail.com>
2020-03-08 20:11:45 -0300

    Update PreferenceJedisMessageListener.java

--- rc.common-shared/src/main/java/com/redecommunity/common/shared/preference/listener/PreferenceJedisMessageListener.java
+++ rc.common-shared/src/main/java/com/redecommunity/common/shared/preference/listener/PreferenceJedisMessageListener.java
@@ -32,7 +32,8 @@ public class PreferenceJedisMessageListener implements JedisMessageListener {
 
         user.togglePreference(
                 preference,
-                action
+                action,
+                false
         );
     }
 }
