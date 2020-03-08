cd3371adb5c02694ccce9416e1686e4eb434dea5
Gutyerrez <srgutyerrez@gmail.com>
2020-03-08 02:29:21 -0300

    Create Cooldown.java

---
+++ rc.common-shared/src/main/java/com/redecommunity/common/shared/cooldown/data/Cooldown.java
@@ -0,0 +1,21 @@
+package com.redecommunity.common.shared.cooldown.data;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Created by @SrGutyerrez
+ */
+@RequiredArgsConstructor
+public class Cooldown<T> {
+    @Getter
+    private final Integer userId;
+    @Getter
+    private final Long endTime;
+    @Getter
+    private final T clazz;
+
+    public Boolean inCooldown() {
+        return this.endTime >= System.currentTimeMillis();
+    }
+}
