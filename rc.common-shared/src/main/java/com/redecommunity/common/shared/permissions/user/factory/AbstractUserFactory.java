c8f8b8d944b066f653497ace32690f51c5cde842
Gutyerrez <srgutyerrez@gmail.com>
2020-03-19 03:55:54 -0300

    Create AbstractUserFactory.java

---
+++ rc.common-shared/src/main/java/com/redecommunity/common/shared/permissions/user/factory/AbstractUserFactory.java
@@ -0,0 +1,14 @@
+package com.redecommunity.common.shared.permissions.user.factory;
+
+import java.util.UUID;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public abstract class AbstractUserFactory<U> {
+    public abstract U getUser(Integer id);
+
+    public abstract U getUser(String username);
+
+    public abstract U getUser(UUID uniqueId);
+}
