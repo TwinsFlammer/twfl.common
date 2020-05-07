9df4d1a70e46897eef8c05a0a05f6b2839cdb53e
Gutyerrez <srgutyerrez@gmail.com>
2020-05-07 03:17:10 -0300

    Update GroupNames.java

--- rc.common-shared/src/main/java/com/redefocus/common/shared/permissions/group/GroupNames.java
+++ rc.common-shared/src/main/java/com/redefocus/common/shared/permissions/group/GroupNames.java
@@ -6,7 +6,6 @@ package com.redefocus.common.shared.permissions.group;
 public class GroupNames {
     public static final String MASTER = "master",
             DIRECTOR = "director",
-            COORDINATOR = "coordinator",
             ADMINISTRATOR = "administrator",
             MODERATOR = "moderator",
             HELPER = "helper",
@@ -16,8 +15,11 @@ public class GroupNames {
             DEFAULT = "default";
 
     @Deprecated
-    public static String MANAGER = GroupNames.DIRECTOR;
+    public static final String MANAGER = GroupNames.DIRECTOR;
 
     @Deprecated
-    public static String ELITE = GroupNames.FARMER;
+    public static final String COORDINATOR = GroupNames.ADMINISTRATOR;
+
+    @Deprecated
+    public static final String ELITE = GroupNames.FARMER;
 }
